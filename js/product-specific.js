4602feb8ffd63f310a59b5852e8a007dba0c55f1
AniTei <115176414+AniTei@users.noreply.github.com>
2023-04-23 21:18:58 +0200

    deleted backups and some console logs

--- js/product-specific.js
+++ js/product-specific.js
@@ -1,51 +1,38 @@
-const specificContainer = document.querySelector (".specific-container");
 
-specificContainer.innerHTML = "halo";
-
+const apiBase = "http://cross-course.local"
+const woocommerceBase = "/wp-json/wc/store"
+const productsBase = "/products/"
 
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const idJacket = params.get("id");
 
-console.log (idJacket);
+const specificContainer = document.querySelector(".specific-container");
 
-
-const url = "http://cross-course.local/wp-json/wc/store/products/" + idJacket; 
-
-
+const urlSpecificJacket = apiBase + woocommerceBase + productsBase + idJacket;
 
 async function findJacket() {
     try {
-        const respond = await fetch(url);
+        const respond = await fetch(urlSpecificJacket);
         const data = await respond.json();
 
-        console.log (data);
+        /* I SHOULD MAYBE JUST RETURN DATA VARIABLE, AND CONTINUE IN NEW FUNCTION*/
 
         specificContainer.innerHTML = "";
 
-       
-
-        specificContainer.innerHTML =`
-
+        specificContainer.innerHTML = `
             <h1>${data.name}</h1>
-
                 <div class="product-in-list spec-grid layout1000">
-
                     <p class="spec-price">${data.price_html}</p>
-
                     <div class="product-img spec-img">
                         <img src="${data.images[0].src}" alt="${data.images[0].alt}">
                     </div> 
-                    
                     ${data.description}
-                   
                     <div class="spec-btns">
                         <a href="checkout.html" class="cta">add to cart</a>
                         <a href="products.html" class="cta cta-back">back</a>
                     </div>
-
-                </div>`
-
+                </div>`;
     }
     catch (error) {
         console.log("OOOPSIE:/sjekk internettforbindelse!", error);
--- js/products.js
+++ js/products.js
@@ -1,126 +1,82 @@
-
-
 const apiBase = "http://cross-course.local"
 const woocommerceBase = "/wp-json/wc/store"
 const productsBase = "/products"
 
-
-
-
-
-/* ///// */
+const urlProducts = apiBase + woocommerceBase + productsBase;
 
 const jacketsContent = document.querySelector(".content-container");
 
-const urlProducts = apiBase + woocommerceBase + productsBase;
-
 async function getJackets() {
-    try {
+    
         const respond = await fetch(urlProducts);
         const data = await respond.json();
 
-        jacketsContent.innerHTML += "";
+        /* I SHOULD MAYBE JUST RETURN DATA VARIABLE, AND CONTINUE IN NEW FUNCTION*/
 
         for (let i = 0; i < data.length; i++) {
 
-            console.log(data[i].id);
-
             jacketsContent.innerHTML += `
-
-
                 <div class="listed-product">
-                
                     <a class="products-top" href="product-specific.html?id=${data[i].id}"> 
-
-                    <div class="list-head">
-                        <h3 class="name-jacket"> ${data[i].name}</h3> 
-                        
-                    </div>
-                
-                    <img src="${data[i].images[0].thumbnail}" alt="${data[i].images[0].alt}" class="product-img list-img">
-                    
-                    <p class="prod-price"> ${data[i].price_html} </p>
+                        <div class="list-head">
+                            <h3 class="name-jacket"> ${data[i].name}</h3>           
+                        </div>                
+                        <img src="${data[i].images[0].thumbnail}" alt="${data[i].images[0].alt}" class="product-img list-img">                    
+                        <p class="prod-price"> ${data[i].price_html} </p>
                     </a>
-
                     <div class="list-btns">
                         <p class="cta">sold out :(</p>
                         <a href="product-specific.html?id=${data[i].id}" class="cta">read more</a>
                     </div>  
-                    
-                </div> `;
-
+                </div> 
+                `;
         }
-
-    }
-
-    catch (error) {
-        console.log("OOOPSIE:/sjekk internettforbindelse!", error);
-        jacketsContent.innerHTML = `<div class="error">OH NO, something went wrong 😢</div>`;
-    }
 }
 
 getJackets();
 
 
-
-
-
-
-
-
-/* ///////////////// */
-
+/* FEATURED ONES  ///////////////// */
 
 
 const featuredContainer = document.querySelector(".featured-content");
 
-const urlFeatured = "http://cross-course.local/wp-json/wc/store/products/?featured=true";
+const featuredBase = "/?featured=true"
 
-/* //// devide up the first url, and use it for the featured part as well  */
+const urlFeatured = urlProducts + featuredBase;
 
 async function getFeatured() {
 
     const respond = await fetch(urlFeatured);
     const dataFeatured = await respond.json();
 
-    console.log(dataFeatured);
-
     for (let i = 0; i < dataFeatured.length; i++) {
 
-        console.log(dataFeatured[i].id);
-
         featuredContainer.innerHTML += `  
-        
         <div class="featured-product">
-        
             <a class="products-top" href="product-specific.html?id=${dataFeatured[i].id}"> 
-
                 <div class="list-head">
                     <h3>${dataFeatured[i].name}</h3>                   
                     <p class="prod-price"> ${dataFeatured[i].price_html} </p>
                 </div>
-            
                 <img src="${dataFeatured[i].images[0].thumbnail}" alt="${dataFeatured[i].images[0].alt}" class="product-img list-img">
-            </a>
-                
+            </a>  
             <div class="list-info">
                 ${dataFeatured[i].short_description}
             </div> 
-
             <div class="list-btns">
                 <p class="cta">sold out :(</p>
                 <a href="product-specific.html?id=${dataFeatured[i].id}" class="cta">read more</a>
             </div> 
-        
-        </div>
-                
+        </div> 
                 `;
     }
 
-
     /* 
-I WEREN'T ABLE TO FILTER THE FEATURED PRODUCTS BASED ON THE FEATURED PROPERTY, BUT I WAS ABLE TO DO IT BY NAME.
-I DECIDED TO JUST USE THE URL FOR FEATURED PRODUCTS, AS IT WILL BE AFFECTED BY THE CHIOCES MADE IN THE CMS, AND USING NAME WOULDN'T HAVE.
+I WEREN'T ABLE TO FILTER THE FEATURED PRODUCTS BASED ON THE FEATURED PROPERTY, 
+BUT I WAS ABLE TO DO IT BY NAME.
+I DECIDED TO JUST USE THE URL FOR FEATURED PRODUCTS, 
+BECAUSE IT WILL BE AFFECTED BY THE CHIOCES MADE IN THE CMS, AND FILTERING BY NAME WOULDN'T.
 
         const filteredJackets = data.filter(filterJackets)
     
@@ -137,7 +93,6 @@ I DECIDED TO JUST USE THE URL FOR FEATURED PRODUCTS, AS IT WILL BE AFFECTED BY T
 
 }
 getFeatured();
-
 
 
 
